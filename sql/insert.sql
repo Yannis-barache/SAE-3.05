aff52611874de4222e2f8b511d71a2512364e4d4
Yannis-barache <ybarache@icloud.com>
2023-10-23 18:44:03 +0200

    Ajout d'insert certain

---
+++ sql/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO ARMES values
+        (1,'fleuret homme'),
+        (2,'fleuret femme'),
+        (3,'sabre homme'),
+        (4,'sabre femme'),
+        (5,'épée homme'),
+        (6,'épée femme');
+
+INSERT INTO CATEGORIE values
+        (1,'U13'),
+        (2,'U15'),
+        (3,'U17'),
+        (4,'U20'),
+        (5,'Senior'),
+        (6,'V1'),
+        (7,'V2'),
+        (8,'V3'),
+        (9,'V4');
+
+
+
+
+
