26f93a85cfa78b04b93886edbb4c326ac9d3e42d
Yannis-barache <ybarache@icloud.com>
2023-10-23 21:58:44 +0200

    ajout insert CLUB et ORGANISATEUR

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO ARMES values
+INSERT INTO ARMES(idArme, nomArme) values
         (1,'fleuret homme'),
         (2,'fleuret femme'),
         (3,'sabre homme'),
@@ -6,7 +6,7 @@ INSERT INTO ARMES values
         (5,'épée homme'),
         (6,'épée femme');
 
-INSERT INTO CATEGORIE values
+INSERT INTO CATEGORIE(idCategorie, nomCategorie) values
         (1,'U13'),
         (2,'U15'),
         (3,'U17'),
@@ -17,6 +17,28 @@ INSERT INTO CATEGORIE values
         (8,'V3'),
         (9,'V4');
 
+INSERT INTO CLUB(idClub, nomClub, adresse, mdpClub) values
+        (1,'Club d''escrime de Paris quai de seine','3 Quai Saint-Bernard 75005 Paris','lesquais'),
+        (2,'Club d''escrime de Lyon','215 Rue Paul Bert, 69003 Lyon','leslions'),
+        (3,'Club d''escrime de Marseille','33 Rue Crillon, 13005 Marseille','marseille'),
+        (4,'Club d''escrime de Bordeaux','53 Rue Pauline Kergomard, 33800 Bordeaux','levin'),
+        (5,'TUC Escrime','6 Rue Claudius Rougenet, 31500 Toulouse','lessaucisses'),
+        (6,'Académie vauban','Pl. Nouvelle Aventure, 59000 Lille','vauban'),
+        (7,'NEC','Rue Louis Joxe, 44200 Nantes','mantes'),
+        (8,'SUC','43 Av. du Rhin, 67100 Strasbourg','presqueallemagne'),
+        (9,'MUC','60 Pl. Jacques Mirouze, 34000 Montpellier','sud'),
+        (10,'Club d''escrime de Rennes','8 Av. des Gayeulles, 35700 Rennes','villedeKnou'),
+        (11,'Club d''escrime de Reims','Imp. Léo Lagrange, 51100 Reims','et'),
+        (12,'Club d''escrime de Saint-Etienne','Rue du Dr Paul Michelon, 42000 Saint-Étienne','perdant');
+
+
+INSERT INTO ORGANISATEUR(idOrganisateur, nomOrganisateur, prenomOrganisateur, adresseMailOrganisateur, mdpOrganisateur, nomUtilisateur) values
+        (1,'BARACHE','Yannis','yannis.barache1@gmail.com','yannis','barache'),
+        (2,'FOFANA','Abdoulahi','abdoulahifofana10@gmail.com','abdoulahi','fofana'),
+        (3,'MERIT','Juliann','juliann.merit@gmail.com','juliann','merit'),
+        (4,'CHEDEVILLE','Baptiste','baba.chedeville45@gmail.com','baptiste','chedeville'),
+        (5,'ELKASMI','Hajar','elks.hajar@gmail.com','hajar','elkasmi'),
+        (6,'LECHOPIER','Christophe','christophe.lechopier@univ-orleans.fr','christophe','lechopier');
 
 
 
