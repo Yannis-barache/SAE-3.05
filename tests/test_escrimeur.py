7b2597b723a6eaa190f8c27d617d176082033dd9
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2023-10-24 14:01:54 +0200

    fix: ajout des attributs sexe et categorie dans un escrimeur

--- tests/test_escrimeur.py
+++ tests/test_escrimeur.py
@@ -12,6 +12,8 @@ sys.path.append(os.path.join(ROOT, 'appli/modele'))
 from escrimeur import Escrimeur
 from club import Club
 
+from categorie import Categorie
+
 
 class TestEscrimeur(unittest.TestCase):
     """
@@ -23,9 +25,10 @@ class TestEscrimeur(unittest.TestCase):
         Test du constructeur de la classe Escrimeur
         """
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        categorie = Categorie(1, 'U19')
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertIsInstance(escrimeur, Escrimeur)
 
     def test_get_id(self):
@@ -33,86 +36,105 @@ class TestEscrimeur(unittest.TestCase):
         Test de la fonction get_id de la classe Escrimeur
         """
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        categorie = Categorie(1, 'U19')
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_id(), 1)
 
     def test_get_nom(self):
         """
         Test de la fonction get_nom de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_nom(), 'Chédeville')
 
     def test_get_prenom(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_prenom(), 'Baptiste')
 
-    def test_get_date_naissance(self):
+    def test_get_sexe(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
+        self.assertEqual(escrimeur.get_sexe(), 'M')
+
+    def test_get_date_naissance(self):
+        categorie = Categorie(1, 'U19')
+        club = Club(1, 'Club 1', 'Adresse 1')
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
+                              'baptched', 'baptiste.chedeville@gmail.com',
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_date_naissance(), '2004-14-05')
 
     def test_get_nom_utilisateur(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_nom_utilisateur(), 'baptched')
 
     def test_get_adresse_mail(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_adresse_mail(),
                          'baptiste.chedeville@gmail.com')
 
     def test_get_mdp(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_mdp(), 'mdp')
 
     def test_get_licence(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_licence(), 'AB21')
 
     def test_get_classement(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_classement(), 1)
 
     def test_get_club(self):
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(escrimeur.get_club(), club)
 
     def test_set_id(self):
         """
         Test de la fonction set_id de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_id(2)
         self.assertEqual(escrimeur.get_id(), 2)
 
@@ -120,10 +142,11 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_nom de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_nom('Chedeville')
         self.assertEqual(escrimeur.get_nom(), 'Chedeville')
 
@@ -131,21 +154,35 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_prenom de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_prenom('Eric')
         self.assertEqual(escrimeur.get_prenom(), 'Eric')
+
+    def test_set_sexe(self):
+        """
+        Test de la fonction set_sexe de la classe Escrimeur
+        """
+        club = Club(1, 'Club 1', 'Adresse 1')
+        categorie = Categorie(1, 'U19')
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
+                              'baptched', 'baptiste.chedeville@gmail.com',
+                              'mdp', 'AB21', 1, club, categorie)
+        escrimeur.set_sexe('F')
+        self.assertEqual(escrimeur.get_sexe(), 'F')
 
     def test_set_date_naissance(self):
         """
         Test de la fonction set_date_naissance de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_date_naissance('2004-05-14')
         self.assertEqual(escrimeur.get_date_naissance(), '2004-05-14')
 
@@ -153,10 +190,11 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_nom_utilisateur de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_nom_utilisateur('bapt')
         self.assertEqual(escrimeur.get_nom_utilisateur(), 'bapt')
 
@@ -164,10 +202,11 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_adresse_mail de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_adresse_mail('baptiste@gmail.com')
         self.assertEqual(escrimeur.get_adresse_mail(), 'baptiste@gmail.com')
 
@@ -175,10 +214,11 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_mdp de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_mdp('motdepasse')
         self.assertEqual(escrimeur.get_mdp(), 'motdepasse')
 
@@ -186,10 +226,11 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_licence de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_licence('AB22')
         self.assertEqual(escrimeur.get_licence(), 'AB22')
 
@@ -197,10 +238,11 @@ class TestEscrimeur(unittest.TestCase):
         """
         Test de la fonction set_classement de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         escrimeur.set_classement('2')
         self.assertEqual(escrimeur.get_classement(), '2')
 
@@ -209,23 +251,38 @@ class TestEscrimeur(unittest.TestCase):
         Test de la fonction set_club de la classe Escrimeur
         """
         club1 = Club(1, 'Club 1', 'Adresse 1')
+        categorie = Categorie(1, 'U19')
         club2 = Club(2, 'Club 2', 'Adresse 2')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club1)
+                              'mdp', 'AB21', 1, club1, categorie)
         escrimeur.set_club(club2)
         self.assertEqual(escrimeur.get_club(), club2)
+
+    def test_set_categorie(self):
+        """
+        Test de la fonction set_categorie de la classe Escrimeur
+        """
+        club = Club(1, 'Club 1', 'Adresse 1')
+        categorie1 = Categorie(1, 'U19')
+        categorie2 = Categorie(2, 'U20')
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
+                              'baptched', 'baptiste.chedeville@gmail.com',
+                              'mdp', 'AB21', 1, club, categorie1)
+        escrimeur.set_categorie(categorie2)
+        self.assertEqual(escrimeur.get_categorie(), categorie2)
 
     def test_str(self):
         """
         Test de la fonction __str__ de la classe Escrimeur
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur = Escrimeur(2, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                               'baptched', 'baptiste.chedeville@gmail.com',
-                              'mdp', 'AB21', 1, club)
+                              'mdp', 'AB21', 1, club, categorie)
         self.assertEqual(
             str(escrimeur),
             '2 - Chédeville - Baptiste - 2004-14-05 - baptched - '
-            'baptiste.chedeville@gmail.com - mdp - AB21 - 1 - 1 - Club 1 - Adresse 1'
+            'baptiste.chedeville@gmail.com - mdp - AB21 - 1 - 1 - Club 1 - Adresse 1 - 1 - U19'
         )
--- tests/test_match.py
+++ tests/test_match.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.join(ROOT, 'appli/modele'))
 from match import Match
 from club import Club
 from escrimeur import Escrimeur
+from categorie import Categorie
 
 
 class TestMatch(unittest.TestCase):
@@ -27,12 +28,13 @@ class TestMatch(unittest.TestCase):
         Test du constructeur de la classe Match
         """
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        categorie = Categorie(1, 'U19')
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         self.assertIsInstance(match, Match)
 
@@ -41,12 +43,13 @@ class TestMatch(unittest.TestCase):
         Test de la fonction get_id de la classe Match
         """
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        categorie = Categorie(1, 'U19')
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         self.assertEqual(match.get_id(), 1)
 
@@ -54,13 +57,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction get_id_phase de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         self.assertEqual(match.get_id_phase(), 1)
 
@@ -68,13 +72,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction get_escrimeur1 de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         self.assertEqual(match.get_escrimeur1(), escrimeur1)
 
@@ -82,13 +87,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction get_escrimeur2 de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         self.assertEqual(match.get_escrimeur2(), escrimeur2)
 
@@ -96,13 +102,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction set_id de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         match.set_id(2)
         self.assertEqual(match.get_id(), 2)
@@ -111,13 +118,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction set_id_phase de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         match.set_id_phase(2)
         self.assertEqual(match.get_id_phase(), 2)
@@ -126,13 +134,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction set_escrimeur1 de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         match.set_escrimeur1(escrimeur2)
         self.assertEqual(match.get_escrimeur1(), escrimeur2)
@@ -141,13 +150,14 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction set_escrimeur2 de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         match.set_escrimeur2(escrimeur1)
         self.assertEqual(match.get_escrimeur2(), escrimeur1)
@@ -156,17 +166,19 @@ class TestMatch(unittest.TestCase):
         """
         Test de la fonction __str__ de la classe Match
         """
+        categorie = Categorie(1, 'U19')
         club = Club(1, 'Club 1', 'Adresse 1')
-        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', '2004-14-05',
+        escrimeur1 = Escrimeur(1, 'Chédeville', 'Baptiste', 'M', '2004-14-05',
                                'baptched', 'baptiste.chedeville@gmail.com',
-                               'mdp', 'AB21', 1, club)
-        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', '2004-14-05', 'clb',
-                               'Evelin.Colomban@gmail.com', 'mdp', 'AB11', 2,
-                               club)
+                               'mdp', 'AB21', 1, club, categorie)
+        escrimeur2 = Escrimeur(2, 'Evelin', 'Colomban', 'M', '2004-14-05',
+                               'clb', 'Evelin.Colomban@gmail.com', 'mdp',
+                               'AB11', 2, club, categorie)
         match = Match(1, 1, escrimeur1, escrimeur2)
         self.assertEqual(
             str(match),
             '1 - 1 - 1 - Chédeville - Baptiste - 2004-14-05 - baptched - '
             'baptiste.chedeville@gmail.com - mdp - AB21 - 1 - 1 - Club 1 - Adresse 1 - '
-            '2 - Evelin - Colomban - 2004-14-05 - clb - Evelin.Colomban@gmail.com - '
-            'mdp - AB11 - 2 - 1 - Club 1 - Adresse 1')
+            '1 - U19 - 2 - Evelin - Colomban - 2004-14-05 - clb - '
+            'Evelin.Colomban@gmail.com - mdp - AB11 - 2 - 1 - Club 1 - Adresse 1 - 1 - U19'
+        )
