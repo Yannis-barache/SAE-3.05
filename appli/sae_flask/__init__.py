1f6fef21d75e848840fa050bba915dc9f3a72675
Abdoulahi Fofana <abdoulahi.fofana@etu.univ-orleans.fr>
2023-10-25 10:30:13 +0200

    mise en place de l'environnement flask

---
+++ appli/sae_flask/__init__.py
@@ -0,0 +1,2 @@
+from .app import app
+import sae_flask.views
---
+++ appli/sae_flask/views.py
@@ -0,0 +1,14 @@
+from .app import app
+from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template(
+        "home.html"
+    )
+
+@app.route("/connexion")
+def connexion():
+    return render_template(
+        "page_connexion.html"
+    )
