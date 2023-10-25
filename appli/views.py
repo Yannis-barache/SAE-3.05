b9dce87231a15a8508ddc1dcad0256e872deac79
Yannis-barache <ybarache@icloud.com>
2023-10-25 12:09:48 +0200

    Suppression de la route "/"

--- appli/views.py
+++ appli/views.py
@@ -3,21 +3,10 @@ from flask import render_template, url_for , redirect
 from appli.modele import *
 from .commands import *
 from .views import *
-from .models import *
+from .modele.connexion_bd import ConnexionBD
 
 user = "332948"
 passwd = "SAE-EscrimeBUT2&"
 host = "mysql-sae-escrime.alwaysdata.net"
 database = "sae-escrime_sae"
-
-connexion=ouvrir_connexion(user,passwd,host,database)
-
-@app.route('/')
-def home():
-    recup=get_orga(connexion)
-    organisateurs=[]
-    for orga in recup:
-        organisateurs.append(orga.get_nom())
-
-    return render_template('home.html', organisateurs=organisateurs)
 
