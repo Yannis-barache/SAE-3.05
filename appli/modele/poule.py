1e7adc8d08a5f1caaaca61ed8eef52dfaaeb0505
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2023-10-25 14:54:23 +0200

    exploit: implémentation de la génération des matchs pour une poule

--- appli/modele/poule.py
+++ appli/modele/poule.py
@@ -2,6 +2,9 @@
 Module contenant la classe Poule
 """
 
+from escrimeur import Escrimeur
+from match import Match
+
 
 class Poule:
     """
@@ -28,6 +31,31 @@ class Poule:
             id_poule (int): id de la poule
         """
         self.__id = id_poule
+
+    def generer_matchs(self, infos: tuple[Escrimeur, list[Escrimeur]],
+                       heure_debut: float) -> list[Match]:
+        """
+        Fonction qui genere les matchs de la poule
+
+        Args:
+            infos (tuple[Escrimeur, list[Escrimeur]]): Les infos de la poule
+
+        Returns:
+            list[Match]: liste des matchs de la poule
+        """
+        les_matchs = []
+        arbitre, les_escrimeurs = infos
+        for escrimeur1 in les_escrimeurs:
+            for escrimeur2 in les_escrimeurs:
+                if escrimeur1 != escrimeur2 and escrimeur1.get_id(
+                ) < escrimeur2.get_id():
+                    les_matchs.append(
+                        Match(-1, self.__id, escrimeur1, escrimeur2, arbitre,
+                              heure_debut))
+                    heure_debut += 0.05
+                    if heure_debut % 1 >= 0.6:
+                        heure_debut += 0.4
+        return les_matchs
 
     def __str__(self):
         return f'{self.__id}'
--- tests/test_poule.py
+++ tests/test_poule.py
@@ -10,6 +10,7 @@ ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 sys.path.append(os.path.join(ROOT, 'appli/modele'))
 
 from poule import Poule
+from escrimeur import Escrimeur
 
 
 class TestPoule(unittest.TestCase):
@@ -41,6 +42,29 @@ class TestPoule(unittest.TestCase):
         poule = Poule(1)
         poule.set_id(2)
         self.assertEqual(poule.get_id(), 2)
+
+    def test_generer_matchs(self):
+        """
+        Test de la fonction generer_matchs de la classe Poule
+        """
+        poule = Poule(1)
+        arbitre = Escrimeur(1, 'nom', 'prenom', 'sexe', 'date_naissance',
+                            'mdp', 'email', 'telephone', 'licence', None, None)
+        escrimeur1 = Escrimeur(2, 'nom', 'prenom', 'sexe', 'date_naissance',
+                               'mdp', 'email', 'telephone', 'licence', None,
+                               None)
+        escrimeur2 = Escrimeur(3, 'nom', 'prenom', 'sexe', 'date_naissance',
+                               'mdp', 'email', 'telephone', 'licence', None,
+                               None)
+        escrimeur3 = Escrimeur(4, 'nom', 'prenom', 'sexe', 'date_naissance',
+                               'mdp', 'email', 'telephone', 'licence', None,
+                               None)
+        escrimeur4 = Escrimeur(5, 'nom', 'prenom', 'sexe', 'date_naissance',
+                               'mdp', 'email', 'telephone', 'licence', None,
+                               None)
+        infos = (arbitre, [escrimeur1, escrimeur2, escrimeur3, escrimeur4])
+        heure_debut = 10.5
+        self.assertEqual(len(poule.generer_matchs(infos, heure_debut)), 6)
 
     def test_str(self):
         """
