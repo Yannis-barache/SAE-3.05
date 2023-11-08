3fa625f7863380e57cb9b135a91d5e148752de1f
Juliann MERIT <juliann.merit@gmail.com>
2023-11-08 17:01:22 +0100

    changement système de variable pour le menu choix

--- launch.bat
+++ launch.bat
@@ -3,12 +3,12 @@
 call venv\Scripts\activate.bat
 cd appli/sae_flask
 python menu_choix.py
-cd ../modele
+cd ../..
 
 set "vide= True"
-for /f "tokens=2 delims==" %%i in ('findstr /i "locale" constantes.py') do set "locale=%%i"
+for /f "tokens=2 delims==" %%i in ('findstr /i "locale" config.ini') do set "locale=%%i"
 if not "%locale%"=="%vide%" (
-    cd ..
+    cd appli
     start http://127.0.0.1:5000
     flask run
 )
