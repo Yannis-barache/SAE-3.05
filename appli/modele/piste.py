a6c68f1cb40107b2b0a5293e82d1b62fe78e0c11
Yannis-barache <ybarache@icloud.com>
2023-12-18 08:51:57 +0100

    fix du pylint

--- appli/modele/piste.py
+++ appli/modele/piste.py
@@ -1,14 +1,15 @@
 """
 Module contenant la classe Piste
 """
+from lieu import Lieu
 
 
 class Piste:
     """
-    Classe
+    Classe Piste
     """
 
-    def __init__(self, id_piste: int, id_lieu: int, description_piste: str):
+    def __init__(self, id_piste: int, lieu: Lieu, description_piste: str):
         """
         Constructeur de la classe Piste
         :param id_piste: id de la piste
@@ -16,52 +17,52 @@ class Piste:
         :param description_piste: description de la piste
         """
         self.__id = id_piste
-        self.__id_lieu = id_lieu
+        self.__lieu = lieu
         self.__description = description_piste
 
     def get_id(self):
         """
-        Fonction qui retourne l'id de la categorie
+        Fonction qui retourne l'id de la piste
 
         Returns:
-            int: id de la categorie
+            int: id de la piste
         """
         return self.__id
 
-    def get_id_lieu(self):
+    def get_lieu(self):
         """
         Fonction qui retourne l'id du lieu
 
         Returns :
             int : id du lieu
         """
-        return self.__id_lieu
+        return self.__lieu
 
     def set_description(self, description: str):
         """
-        Fonction qui modifie la description de la categorie
+        Fonction qui modifie la description de la piste
 
         Args:
-            description (str): description de la categorie
+            description (str): description de la piste
         """
         self.__description = description
 
 
     def get_description(self):
         """
-        Fonction qui retourne la description de la categorie
+        Fonction qui retourne la description de la piste
 
         Returns :
-            str : description de la categorie
+            str : description de la piste
         """
         return self.__description
 
     def __str__(self):
         """
-        Fonction qui retourne une chaine de caractere contenant les informations de la categorie
+        Fonction qui retourne une chaine de caractere contenant les informations de la piste
 
         Returns :
-            str : informations de la categorie
+            str : informations de la piste
         """
-        return ("Piste : id = " + str(self.__id) + ", idLieu = " + str(self.__idLieu) +
-                ", description = " + str(self.__descriptionPiste))
+        return ("Piste : id = " + str(self.__id) + ", idLieu = " + str(self.__lieu) +
+                ", description = " + str(self.__description))
