c6797fc8b1b1bfd3fd7ff8c382461fa9475c34b3
Baptiste Chedeville <130366929+Baptched@users.noreply.github.com>
2023-12-19 17:22:21 +0100

    exploit

--- appli/BD/match_bd.py
+++ appli/BD/match_bd.py
@@ -9,7 +9,6 @@ from phase_bd import PhaseBD
 from escrimeur_bd import EscrimeurBD
 from inscrire_arbitre_bd import InscrireArbitreBD
 from piste_bd import PisteBD
-from touche_bd import ToucheBD
 
 ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../..')
 sys.path.append(os.path.join(ROOT, 'appli/modele'))
@@ -80,7 +79,6 @@ class MatchBD:
                 piste = PisteBD(self.__connexion).get_piste_by_id(id_piste)
                 match = Match(id_match, phase, escrimeur1, escrimeur2, arbitre,
                               heure, fini, piste)
-                match.set_touche(ToucheBD(self.__connexion).get_by_match(match))
                 return match
             return None
         except Exception as e:
