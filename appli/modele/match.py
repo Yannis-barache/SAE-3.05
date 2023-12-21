fef8e62e759f3d0d574d4b02954ea7a909fc9076
Abdoulahi Fofana <abdoulahi.fofana@etu.univ-orleans.fr>
2023-12-21 09:55:00 +0100

    amélioration css page

--- appli/modele/match.py
+++ appli/modele/match.py
@@ -82,6 +82,19 @@ class Match:
             float: heure du match
         """
         return self.__heure
+    
+    def get_heure_str(self) -> str:
+        """
+        Fonction qui retourne l'heure du match sous forme de chaîne de caractères
+
+        Returns:
+            str: heure du match au format HH:mm
+        """
+        heures, reste = divmod(self.__heure.seconds, 3600)
+        minutes = reste // 60
+
+        format_horloge = f"{heures:02d}h{minutes:02d}"
+        return format_horloge
 
     def get_type_phase(self) -> str | None:
         """
